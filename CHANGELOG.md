eafd294ce8042d8545c47e6b34bc99b43bcf8e6a
raymelon <franciscoraymel@gmail.com>
2017-11-13 00:28:42 +0800

    [FEATURE] (v0.1.21-pre) build 29. Added testing modules.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,13 @@
 
 [lib/tests/annotator_unit_test.py](https://github.com/raymelon/Eliana/blob/0.1-pre/tests/annotator_unit_test.py)
 - Changed import dependencies from adding lib paths to syspath to relative imports.
+
+
+### [0.1.21-pre build 29 (, 11-12-17)]
+
+[tests/eliana_test.py](https://github.com/raymelon/Eliana/blob/0.1-pre/tests/eliana_test.py)
+- Initial commit for this file.
+
+[tests/annotator_unit_test.py](https://github.com/raymelon/Eliana/blob/0.1-pre/tests/annotator_unit_test.py)
+- Used the classes provided by tests/eliana_test.py to modularize testing.
+
---
+++ tests/eliana_test.py
@@ -0,0 +1,119 @@
+"""
+.. module:: eliana_test
+    :synopsis: base module for tests
+
+.. moduleauthor:: Raymel Francisco <franciscoraymel@gmail.com>
+.. created:: Nov 12, 2017
+"""
+
+import os
+from abc import ABC, abstractmethod
+
+class ElianaTest(ABC):
+    """.. class:: ElianaTest
+
+    Base module for all types of tests.
+    """
+    def __init__(self):
+        self.dir_working = os.getcwd()
+
+        self.__dir_env_modules = os.path.join(
+            self.dir_working,
+            'env',
+            'eliana',
+            'lib',
+            'python3.6',
+            'site-packages'
+        )
+        self.__dir_local_modules = os.path.join(
+            self.dir_working,
+            'lib'
+        )
+        self.eliana_log = ElianaLog()
+
+    @property
+    def dir_env_modules(self):
+        return self.__dir_env_modules
+
+    @dir_env_modules.setter
+    def dir_env_modules(self, dir_):
+        self.__dir_env_modules = dir_
+
+    @property
+    def dir_local_modules(self):
+        return self.__dir_local_modules
+
+    @dir_local_modules.setter
+    def dir_local_module(self, dir_):
+        self.__dir_local_modules = dir_
+
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class ElianaUnitTest(ElianaTest):
+    """.. class:: ElianaUnitTest
+
+    Base module for unit tests.
+    """
+    def __init__(self):
+        ElianaTest.__init__(self)
+
+
+class ElianaIntegratedtTest(ElianaTest):
+    """.. class:: ElianaIntegratedTest
+
+    Base module for integrated tests.
+    """
+    def __init__(self):
+        ElianaTest.__init__(self)
+
+
+class ElianaLog:
+
+    def __init__(self):
+        self.__log_ok = self.__status_formatter('OK')
+        self.__log_error = self.__status_formatter('ERROR')
+        self.__log_warning = self.__status_formatter('WARNING')
+
+        self.__steps = 0
+        self.__step_counter = 1
+
+    @property
+    def steps(self):
+        return self.__steps
+
+    @steps.setter
+    def steps(self, steps):
+        self.__steps = steps
+
+    @property
+    def log_ok(self):
+        return self.__log_ok
+
+    @property
+    def log_error(self):
+        return self.__log_error
+
+    @property
+    def log_warning(self):
+        return self.__log_warning
+
+    def __status_formatter(self, log_string) -> str:
+        return ' [' + log_string + ']'
+
+    def log(self, log_string):
+        print(
+            log_string,
+            ' (',
+            self.__step_counter,
+            '/',
+            self.__steps,
+            ')',
+            '...',
+
+            end='',
+            sep=''
+        )
+        self.__step_counter += 1
