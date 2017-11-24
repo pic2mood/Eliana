2a2fd2e5f4e8b30cf864fe1612e27fe4cd2edf27
raymelon <franciscoraymel@gmail.com>
2017-11-24 20:40:12 +0800

    [FEATURE] v0.1.56-pre build 79. Initial eliana module.

---
+++ eliana/lib/eliana.py
@@ -0,0 +1,22 @@
+"""
+.. module:: eliana
+    :synopsis: main module for eliana package
+
+.. moduleauthor:: Raymel Francisco <franciscoraymel@gmail.com>
+.. created:: Nov 24, 2017
+"""
+import numpy as np
+
+# from PIL import Image
+# from eliana.lib.eliana_image import ElianaImage
+
+from skimage.feature import greycomatrix, greycoprops
+
+import scipy.ndimage
+
+
+class Eliana:
+
+    def __init__(self):
+       pass
+
