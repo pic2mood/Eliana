7baf0a6e821f8e71df21db434256dba91676c555
raymelon <franciscoraymel@gmail.com>
2017-12-02 19:46:28 +0800

    [FEATURE] v0.1.65-pre build 88. (#15) Added colorfulness level feature on Color module.
    Chose and implemented option 2, which is to calculate colorfulness level.

--- eliana/lib/color.py
+++ eliana/lib/color.py
@@ -7,6 +7,7 @@
 """
 from PIL import Image
 from eliana.lib.eliana_image import ElianaImage
+import numpy as np
 
 
 class Color:
@@ -18,3 +19,29 @@ class Color:
     @staticmethod
     def to_rgb(img: ElianaImage):
         return ElianaImage(pil=img.as_pil.convert('RGB'))
+
+    @staticmethod
+    def colorfulness(img: ElianaImage):
+
+        r, g, b = img.as_pil.split()
+        # r = ElianaImage(np=r).as_numpy
+        # g = ElianaImage(np=g).as_numpy
+        # b = ElianaImage(np=b).as_numpy
+
+        r = np.array(r.getdata())
+        g = np.array(g.getdata())
+        b = np.array(b.getdata())
+
+        # print(r.__class__)
+
+        rg = np.absolute(r - g)
+        yb = np.absolute(0.5 * (r + g) - b)
+
+        (rb_mean, rb_std) = (np.mean(rg), np.std(rg))
+        (yb_mean, yb_std) = (np.mean(yb), np.std(yb))
+
+        std_root = np.sqrt((rb_std ** 2) + (yb_std ** 2))
+        mean_root = np.sqrt((rb_mean ** 2) + (yb_mean ** 2))
+
+        return std_root + (0.3 * mean_root)
+
